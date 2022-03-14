d3446c25df894723eec6fdbb7d6e964d0935df77
Anubhav <bansalan@arcesium.com>
2022-03-14 16:31:00 +0530

    changes for input file and new test cases till create product

--- test_aspireapp.py
+++ test_aspireapp.py
@@ -2,6 +2,9 @@
 
 import unittest
 import util as util
+import pytest
+import inspect
+import logging
 
 
 class TestAspireapp(unittest.TestCase):
@@ -23,16 +26,22 @@ class TestAspireapp(unittest.TestCase):
     # 3. From the top-menu bar, select `Products -> Products` item, then create a new product
     def test_createProduct(self):
         self.ut.createProduct()
-        print((self.ut.locator['newProdPage']['newProdName']).format(self.ut.input['createProduct']['name']))
-        assert self.ut.validateElement((self.ut.locator['newProdPage']['newProdName']).format(self.ut.input['createProduct']['name']))
+        assert self.ut.validateElement((self.ut.locator['prodInfoPage']['pageHeading']).
+                                       format(self.ut.input['createProduct']['name']))
 
     # 4. Update the quantity of new product is more than 10
     def test_updateProductQuantity(self):
-        assert True
+        self.ut.updateQty()
+        assert self.ut.validateTextInElement((self.ut.locator['prodInfoPage']['countedQtySaved']).
+                                             format(self.ut.input['updateQty']['qty']),
+                                             self.ut.input['updateQty']['qty'])
 
     # 5. From top-left page, click on `Application` icon
     # 6. Navigate to `Manufacturing` feature, then create a new Manufacturing Order item for the created Product on step #3
+    @pytest.mark.slow
     def test_createMafOrder(self):
+        logging.info('Starting Test : ' + inspect.stack()[0][3])
+        self.ut.createMafOrd()
         assert True
 
     # 7. Update the status of new Orders to ??Done?? successfully
--- util.py
+++ util.py
@@ -1,7 +1,8 @@
 # Author: Anubhav
 import json
-
-# from time import sleep
+import logging
+# import os
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,26 +19,79 @@ class Util:
         self.locator = json.load(open('xpath.json'))
         self.input = json.load(open('input.json'))
 
+    def getElement(self, loc):
+        return self.driver.find_element(By.XPATH, value=loc)
+
     def login(self):
         self.driver.get(config.baseURL)
-        page_loc = self.locator['loginPage']
-        self.driver.find_element(By.XPATH, value=page_loc['email']).send_keys(config.username + Keys.TAB)
-        self.driver.find_element(By.XPATH, value=page_loc['password']).send_keys(config.password + Keys.TAB)
-        self.driver.find_element(By.XPATH, value=page_loc['btnLogin']).click()
-
-    def validateElement(self, locator):
-        return self.driver.find_element(By.XPATH, value=locator)
+        self.getElement(self.locator['loginPage']['email']).send_keys(config.username + Keys.TAB)
+        self.getElement(self.locator['loginPage']['password']).send_keys(config.password + Keys.TAB)
+        self.getElement(self.locator['loginPage']['btnLogin']).click()
+        logging.info('Login successful')
 
     def openInventory(self):
         self.login()
-        self.driver.find_element(By.XPATH, value=self.locator['landingPage']['inventory']).click()
+        self.getElement(self.locator['landingPage']['inventory']).click()
 
     def createProduct(self):
         self.openInventory()
-        self.driver.find_element(By.XPATH, value=self.locator['inventoryPage']['menuProducts']).click()
-        self.driver.find_element(By.XPATH, value=self.locator['inventoryPage']['optProducts']).click()
+        self.getElement(self.locator['inventoryPage']['menuProducts']).click()
+        self.getElement(self.locator['inventoryPage']['optProducts']).click()
 
         assert self.validateElement(self.locator['productsPage']['pageHeading'])
-        self.driver.find_element(By.XPATH, value=self.locator['productsPage']['createProd']).click()
-        self.driver.find_element(By.XPATH, value=self.locator['newProdPage']['newProdPage']). \
+        self.getElement(self.locator['productsPage']['createProd']).click()
+        self.input['createProduct']['name'] += str(int(time.time()))
+        self.getElement(self.locator['prodInfoPage']['pageHeading']). \
             send_keys(self.input['createProduct']['name'])
+        self.getElement(self.locator['prodInfoPage']['btnSave']).click()
+        logging.info('Created Product : ' + self.input['createProduct']['name'])
+
+    def updateQty(self):
+        self.createProduct()
+        self.getElement(self.locator['inventoryPage']['menuProducts']).click()
+        self.getElement(self.locator['inventoryPage']['optProducts']).click()
+        self.getElement(self.locator['productsPage']['searchBox']). \
+            send_keys(self.input['createProduct']['name'] + Keys.RETURN)
+        time.sleep(1)  # to handle late loading
+        self.getElement(self.locator['productsPage']['firstRes']).click()
+        self.getElement(self.locator['prodInfoPage']['updateQty']).click()
+        time.sleep(1)
+        self.getElement(self.locator['prodInfoPage']['createQty']).click()
+        self.getElement(self.locator['prodInfoPage']['countedQty']).send_keys(Keys.BACKSPACE * 5)
+        self.getElement(self.locator['prodInfoPage']['countedQty']).send_keys(self.input['updateQty']['qty'])
+        self.getElement(self.locator['prodInfoPage']['saveQty']).click()
+
+    def createMafOrd(self):
+        self.updateQty()
+        self.getElement(self.locator['landingPage']['appList']).click()
+        self.getElement(self.locator['landingPage']['manufacturing']).click()
+        assert self.validateElement(self.locator['mafOrdPage']['pageHeading'])
+
+        self.getElement(self.locator['mafOrdPage']['crtMfaOrd']).click()
+        # self.getElement(self.locator['mafOrdPage']['prdName']).send_keys(self.input['createProduct']['name'])
+        self.driver.find_element(By.CSS_SELECTOR, value=self.locator['mafOrdPage']['prdName']). \
+            send_keys(self.input['createProduct']['name'] + "1647253955")
+        time.sleep(1)
+        self.driver.find_element(By.CSS_SELECTOR, value=self.locator['mafOrdPage']['prdName']).send_keys(Keys.TAB)
+        self.getElement(self.locator['mafOrdPage']['prdQty']).send_keys(Keys.BACKSPACE)
+        self.getElement(self.locator['mafOrdPage']['prdQty']).send_keys(self.input['updateQty']['qty'])
+        self.getElement(self.locator['mafOrdPage']['btnConfirm']).click()
+
+    def updateOrdStatus(self):
+        # self.createMafOrd()
+        self.openInventory()
+        self.getElement(self.locator['landingPage']['appList']).click()
+        self.getElement(self.locator['landingPage']['manufacturing']).click()
+        assert self.validateElement(self.locator['mafOrdPage']['pageHeading'])
+
+        self.getElement(self.locator['mafOrdPage']['btnMKD']).click()
+        self.getElement(self.locator['mafOrdPage']['btnOk']).click()
+        time.sleep(10)
+
+    def validateElement(self, locator):
+        return self.getElement(locator)
+
+    def validateTextInElement(self, locator, text):
+        capTxt = self.getElement(locator).text
+        logging.info("Captured Text : " + capTxt + " & Comparison Text : " + text)
+        return self.getElement(locator).text == text
